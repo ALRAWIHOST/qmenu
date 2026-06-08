079628df1f27baebb53b0856356664a772586572
ALRAWIHOST <alrawirepair1@gmail.com>
2026-06-08 15:56:46 +0200

    Add robots txt

---
+++ src/app/robots.ts
@@ -0,0 +1,11 @@
+import { MetadataRoute } from "next";
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: "*",
+      allow: "/",
+    },
+    sitemap: "https://menu-qrcode.de/sitemap.xml",
+  };
+}
